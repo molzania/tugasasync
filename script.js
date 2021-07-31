80e56168604c2ec3be6fc7c19d41eca11e5f1660
molzania <molzania_1507@yahoo.co.id>
2021-07-31 19:12:25 +0700

    commit on 'main'

---
+++ script.js
@@ -0,0 +1,9 @@
+//Soal 1
+// Lakukan pengambilan data yang berasal dari API berikut “https://api.github.com/users/<github_username>”
+
+// Gunakan promise utk mendapatkan data
+
+// Tampilkan data menggunakan DOM minimal nama, gambar profile, bio, followers, followings
+// Tampilkan serapih mungkin menggunakan CSS
+
+//Soal 2
